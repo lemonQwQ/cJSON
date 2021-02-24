4777d0d13a8539af6f952761f1f502199983f823
lemonQwQ <1282920248@qq.com>
2021-02-24 21:38:56 +0800

    函数parse_number、ensure、pow2gt

--- cJSON/cJSON.c
+++ cJSON/cJSON.c
@@ -49,7 +49,7 @@ static char* cJSON_strdup(const char* str) {
 	size_t len;
 	char* copy;
 	len = strlen(str) + 1;
-	if (!(copy = (char*)cjSON_malloc(len))) {
+	if (!(copy = (char*)cJSON_malloc(len))) {
 		return 0;
 	}
 	memcpy(copy, str, len);
@@ -92,4 +92,131 @@ void cJSON_Delete(cJSON *c) {
 		cJSON_free(c);
 		c = next;
 	}
+}
+
+// 解析文本数字，将结果填充到对象item中
+static const char *parse_number(cJSON *item, const char *num) {
+	double n = 0;
+	double sign = 1;
+	double scale = 0;
+	int subscale = 0;
+	int signsubscale = 1;
+
+	// 确认正负
+	if (*num == '-')
+	{
+		sign = -1;
+		num++;
+	}
+	// 判断是否为0
+	if (*num == '0')
+	{
+		num++;
+	}
+	// 判断是否为数字
+	if ((*num >= '1') && (*num <= '9'))
+	{
+		do
+		{
+			n = (n * 10.0) + (*num++ - '0');
+		} while ((*num >= '0') && (*num <= '9'));
+	}
+	// 判断是否为小数
+	if ((*num == '.') && (num[1] >= '0') && (num[1] <= '9'))
+	{
+		num++;
+		do
+		{
+			n = (n  *10.0) + (*num++ - '0');
+			scale--;
+		} while ((*num >= '0') && (*num <= '9'));
+	}
+	// 是否为指数e
+	if ((*num == 'e') || (*num == 'E'))
+	{
+		num++;
+		// 判断指数正负
+		if (*num == '+')
+		{
+			num++;
+		}
+		else if (*num == '-')
+		{
+			signsubscale = -1;
+			num++;
+		}
+		// 判断指数是否为数字
+		while ((*num >= '0') && (*num <= '9'))
+		{
+			subscale = (subscale * 10) + (*num++ - '0');
+		}
+	}
+
+	/* number = +/- number.fraction * 10^+/- exponent */
+	n = sign * n * pow(10.0, (scale + subscale * signsubscale));
+
+	item->valuedouble = n;
+	item->valueint = (int)n;
+	item->type = cJSON_Number;
+
+	return num;
+}
+
+// 获取第一个比x大的2次方幂
+static int pow2gt(int x) { 
+	--x;  
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+#if INTEGER_SIZE & 0x1110 
+	x |= x >> 8;
+#endif
+#if INTEGER_SIZE & 0x1100 
+	x |= x >> 16;
+#endif
+#if INTEGER_SIZE & 0x1000 
+	x |= x >> 32;
+#endif
+	return x+1;
+}
+
+typedef struct {
+	char *buffer;
+	int length;
+	int offset; // 已占用空间
+} printbuffer;
+
+// 重新分配缓冲空间， 至少需要needed个字节
+static char* ensure(printbuffer *p, int needed) {
+	char *newbuffer;
+	int newsize;
+	if (!p || !p->buffer)
+	{
+		return 0;
+	}
+	needed += p->offset;
+	if (needed <= p->length)
+	{
+		return p->buffer + p->offset;
+	}
+
+	newsize = pow2gt(needed);
+	newbuffer = (char*)cJSON_malloc(newsize);
+	if (!newbuffer)
+	{
+		cJSON_free(p->buffer);
+		p->length = 0;
+		p->buffer = 0;
+
+		return 0;
+	}
+	if (newbuffer)
+	{
+		memcpy(newbuffer, p->buffer, p->length);
+	}
+	cJSON_free(p->buffer);
+	p->length = newsize;
+	p->buffer = newbuffer;
+
+	return newbuffer + p->offset;
 }
--- cJSON/cJSON.h
+++ cJSON/cJSON.h
@@ -43,75 +43,74 @@ extern "C" //去寻找C标准的函数
 		void(*free_fn)(void *ptr);
 	} cJSON_Hooks;
 
-	extern void cJSON_InitHooks(cJSON_Hooks* hooks);
-
-/*	extern cJSON *cJSON_Parse(const char *value);
-
-	extern char  *cJSON_Print(const cJSON *item);
-
-	extern char  *cJSON_PrintUnformatted(const cJSON *item);
-
-	extern char *cJSON_PrintBuffered(const cJSON *item, int prebuffer, int fmt);
-
-	extern void   cJSON_Delete(cJSON *c);
-
-
-	extern int	  cJSON_GetArraySize(const cJSON *array);
-
-	extern cJSON *cJSON_GetArrayItem(const cJSON *array, int item);
-
-	extern cJSON *cJSON_GetObjectItem(const cJSON *object, const char *string);
-	extern int cJSON_HasObjectItem(const cJSON *object, const char *string);
-
 	extern const char *cJSON_GetErrorPtr(void);
 
-	extern cJSON *cJSON_CreateNull(void);
-	extern cJSON *cJSON_CreateTrue(void);
-	extern cJSON *cJSON_CreateFalse(void);
-	extern cJSON *cJSON_CreateBool(int b);
-	extern cJSON *cJSON_CreateNumber(double num);
-	extern cJSON *cJSON_CreateString(const char *string);
-	extern cJSON *cJSON_CreateArray(void);
-	extern cJSON *cJSON_CreateObject(void);
+	extern void cJSON_InitHooks(cJSON_Hooks* hooks);
 
-	extern cJSON *cJSON_CreateIntArray(const int *numbers, int count);
-	extern cJSON *cJSON_CreateFloatArray(const float *numbers, int count);
-	extern cJSON *cJSON_CreateDoubleArray(const double *numbers, int count);
-	extern cJSON *cJSON_CreateStringArray(const char **strings, int count);
+	extern void cJSON_Delete(cJSON *c);
 
-	extern void cJSON_AddItemToArray(cJSON *array, cJSON *item);
-	extern void	cJSON_AddItemToObject(cJSON *object, const char *string, cJSON *item);
-	extern void	cJSON_AddItemToObjectCS(cJSON *object, const char *string, cJSON *item);	/* Use this when string is definitely const (i.e. a literal, or as good as), and will definitely survive the cJSON object */
+//	extern cJSON *cJSON_Parse(const char *value);
+//
+//	extern char  *cJSON_Print(const cJSON *item);
+//
+//	extern char  *cJSON_PrintUnformatted(const cJSON *item);
+//
+//	extern char *cJSON_PrintBuffered(const cJSON *item, int prebuffer, int fmt);
+//
+//	extern int	  cJSON_GetArraySize(const cJSON *array);
+//
+//	extern cJSON *cJSON_GetArrayItem(const cJSON *array, int item);
+//
+//	extern cJSON *cJSON_GetObjectItem(const cJSON *object, const char *string);
+//	extern int cJSON_HasObjectItem(const cJSON *object, const char *string);
 
-	extern void cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
-	extern void	cJSON_AddItemReferenceToObject(cJSON *object, const char *string, cJSON *item);
-
-	extern cJSON *cJSON_DetachItemFromArray(cJSON *array, int which);
-	extern void   cJSON_DeleteItemFromArray(cJSON *array, int which);
-	extern cJSON *cJSON_DetachItemFromObject(cJSON *object, const char *string);
-	extern void   cJSON_DeleteItemFromObject(cJSON *object, const char *string);
-
-	extern void cJSON_InsertItemInArray(cJSON *array, int which, cJSON *newitem); /* Shifts pre-existing items to the right. */
-	extern void cJSON_ReplaceItemInArray(cJSON *array, int which, cJSON *newitem);
-	extern void cJSON_ReplaceItemInObject(cJSON *object, const char *string, cJSON *newitem);
-
-	extern cJSON *cJSON_Duplicate(const cJSON *item, int recurse);
-
-	extern cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int require_null_terminated);
-
-	extern void cJSON_Minify(char *json);*/
-
-/*#define cJSON_AddNullToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateNull())
-#define cJSON_AddTrueToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateTrue())
-#define cJSON_AddFalseToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateFalse())
-#define cJSON_AddBoolToObject(object,name,b) cJSON_AddItemToObject(object, name, cJSON_CreateBool(b))
-#define cJSON_AddNumberToObject(object,name,n) cJSON_AddItemToObject(object, name, cJSON_CreateNumber(n))
-#define cJSON_AddStringToObject(object,name,s) cJSON_AddItemToObject(object, name, cJSON_CreateString(s))
-
-#define cJSON_SetIntValue(object,val) ((object) ? (object)->valueint = (object)->valuedouble = (val) : (val))
-#define cJSON_SetNumberValue(object,val) ((object) ? (object)->valueint = (object)->valuedouble = (val) : (val))
-
-#define cJSON_ArrayForEach(pos, head) for(pos = (head)->child; pos != NULL; pos = pos->next)*/
+//	extern cJSON *cJSON_CreateNull(void);
+//	extern cJSON *cJSON_CreateTrue(void);
+//	extern cJSON *cJSON_CreateFalse(void);
+//	extern cJSON *cJSON_CreateBool(int b);
+//	extern cJSON *cJSON_CreateNumber(double num);
+//	extern cJSON *cJSON_CreateString(const char *string);
+//	extern cJSON *cJSON_CreateArray(void);
+//	extern cJSON *cJSON_CreateObject(void);
+//
+//	extern cJSON *cJSON_CreateIntArray(const int *numbers, int count);
+//	extern cJSON *cJSON_CreateFloatArray(const float *numbers, int count);
+//	extern cJSON *cJSON_CreateDoubleArray(const double *numbers, int count);
+//	extern cJSON *cJSON_CreateStringArray(const char **strings, int count);
+//
+//	extern void cJSON_AddItemToArray(cJSON *array, cJSON *item);
+//	extern void	cJSON_AddItemToObject(cJSON *object, const char *string, cJSON *item);
+//	extern void	cJSON_AddItemToObjectCS(cJSON *object, const char *string, cJSON *item);	/* Use this when string is definitely const (i.e. a literal, or as good as), and will definitely survive the cJSON object */
+//
+//	extern void cJSON_AddItemReferenceToArray(cJSON *array, cJSON *item);
+//	extern void	cJSON_AddItemReferenceToObject(cJSON *object, const char *string, cJSON *item);
+//
+//	extern cJSON *cJSON_DetachItemFromArray(cJSON *array, int which);
+//	extern void   cJSON_DeleteItemFromArray(cJSON *array, int which);
+//	extern cJSON *cJSON_DetachItemFromObject(cJSON *object, const char *string);
+//	extern void   cJSON_DeleteItemFromObject(cJSON *object, const char *string);
+//
+//	extern void cJSON_InsertItemInArray(cJSON *array, int which, cJSON *newitem); /* Shifts pre-existing items to the right. */
+//	extern void cJSON_ReplaceItemInArray(cJSON *array, int which, cJSON *newitem);
+//	extern void cJSON_ReplaceItemInObject(cJSON *object, const char *string, cJSON *newitem);
+//
+//	extern cJSON *cJSON_Duplicate(const cJSON *item, int recurse);
+//
+//	extern cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int require_null_terminated);
+//
+//	extern void cJSON_Minify(char *json);
+//
+//#define cJSON_AddNullToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateNull())
+//#define cJSON_AddTrueToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateTrue())
+//#define cJSON_AddFalseToObject(object,name) cJSON_AddItemToObject(object, name, cJSON_CreateFalse())
+//#define cJSON_AddBoolToObject(object,name,b) cJSON_AddItemToObject(object, name, cJSON_CreateBool(b))
+//#define cJSON_AddNumberToObject(object,name,n) cJSON_AddItemToObject(object, name, cJSON_CreateNumber(n))
+//#define cJSON_AddStringToObject(object,name,s) cJSON_AddItemToObject(object, name, cJSON_CreateString(s))
+//
+//#define cJSON_SetIntValue(object,val) ((object) ? (object)->valueint = (object)->valuedouble = (val) : (val))
+//#define cJSON_SetNumberValue(object,val) ((object) ? (object)->valueint = (object)->valuedouble = (val) : (val))
+//
+//#define cJSON_ArrayForEach(pos, head) for(pos = (head)->child; pos != NULL; pos = pos->next)
 
 #ifdef __cplusplus
 }
